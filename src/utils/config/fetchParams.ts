15946e8e96f885f23d97abcc08a90a064f020ff9
alex pacheco <alexpacheco407@gmail.com>
2024-04-18 20:06:18 -0500

    ajuste lenguaje

--- src/utils/config/fetchParams.ts
+++ src/utils/config/fetchParams.ts
@@ -1,3 +1,3 @@
 import { THE_MOVIE_DB_APIKEY } from "@env";
 
-export const params = `?api_key=${THE_MOVIE_DB_APIKEY}&language=es`;
+export const params = `?api_key=${THE_MOVIE_DB_APIKEY}&language=en`;
